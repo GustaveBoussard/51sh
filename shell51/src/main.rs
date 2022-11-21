1793b9f50d9aa02fb4ccddeec3a64871ce9190dd
Gustave Boussard <gustave.boussard@epita.fr>
2022-11-21 15:54:32 +0100

    feat: executing commands no args

--- shell51/src/main.rs
+++ shell51/src/main.rs
@@ -1,3 +1,21 @@
+use std::io::stdin;
+use std::io::stdout;
+use std::io::Write;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+
+    loop {
+        print!(">");
+        stdout().flush().ok();
+
+        let mut input = String::new();
+        stdin().read_line(&mut input).unwrap();
+
+        let command = input.trim();
+
+        let mut child = Command::new(command).spawn().unwrap();
+
+        child.wait().ok();
+    }
 }
