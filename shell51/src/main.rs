7752cb32c52494cfa321eb53cb785ebaaaf54add
gustave boussard <gustave.boussard@epita.fr>
2022-11-22 00:21:29 +0100

    feat: pipes and exit\nfix: 'cd -'

--- shell51/src/main.rs
+++ shell51/src/main.rs
@@ -2,11 +2,15 @@ use std::env;
 use std::io::stdin;
 use std::io::stdout;
 use std::io::Write;
-use std::process::Command;
+use std::process::{ Child, Command, Stdio };
 
 fn main() {
 
-    let mut old_dir = env::current_dir().unwrap().into_os_string().into_string().unwrap();
+    let mut cur_dir = env::current_dir().unwrap()
+                                        .into_os_string()
+                                        .into_string()
+                                        .unwrap();
+    let mut prev_dir = String::new();
     loop {
         print!("51sh > ");
         stdout().flush().ok();
@@ -14,33 +18,73 @@ fn main() {
         let mut input = String::new();
         stdin().read_line(&mut input).unwrap();
 
-        let mut parts = input.trim().split_whitespace();
-        let command = parts.next().unwrap();
-        let args = parts;
+        let mut commands = input.trim().split(" | ").peekable();
+        let mut prev_cmd = None;
 
-        match command {
-            "cd" => {
-                let dir = args.peekable().peek().map_or("/", |x| *x);
-                match dir {
-                    "-" => {
-                        if let Err(e) = env::set_current_dir(&old_dir) {
-                            eprintln!("{}", e);
-                        }
-                    },
-                    dir => {
-                        old_dir = dir.to_string();
-                        if let Err(e) = env::set_current_dir(&dir) {
-                            eprintln!("{}", e);
+        while let Some(command) = commands.next() {
+            let mut parts = command.trim().split_whitespace();
+            let command = parts.next().unwrap();
+            let args = parts;
+
+            match command {
+                "cd" => {
+                    let dir = args.peekable().peek().map_or("/", |x| *x);
+                    match dir {
+                        "-" => {
+                            if let Err(e) = env::set_current_dir(&prev_dir) {
+                                eprintln!("{}", e);
+                            }
+                            else {
+                                prev_dir = String::from(cur_dir);
+                                cur_dir = env::current_dir().unwrap()
+                                        .into_os_string()
+                                        .into_string()
+                                        .unwrap();
+                            }
+                        },
+                        dir => {
+                            if let Err(e) = env::set_current_dir(&dir) {
+                                eprintln!("{}", e);
+                            }
+                            else {
+                                prev_dir = String::from(cur_dir);
+                                cur_dir = String::from(dir);
+                            }
                         }
                     }
+                    prev_cmd = None;
+                },
+                "exit" => return,
+                command => {
+                    let stdin = prev_cmd.map_or(Stdio::inherit(),
+                                |output: Child| Stdio::from(output.stdout
+                                                                  .unwrap()));
+                    let stdout = if commands.peek().is_some() {
+                        Stdio::piped()
+                    } else {
+                        Stdio::inherit()
+                    };
+
+                    let output = Command::new(command)
+                        .args(args)
+                        .stdin(stdin)
+                        .stdout(stdout)
+                        .spawn();
+
+                    match output {
+                        Ok(output) => { prev_cmd = Some(output); },
+                        Err(e) => {
+                            prev_cmd = None;
+                            eprintln!("{}", e);
+                        },
+                    };
                 }
-            },
-            command => {
-                let child = Command::new(command).args(args).spawn();
-                match child {
-                    Ok(mut child) => { child.wait().ok(); },
-                    Err(e) => eprintln!("{}", e),
-                };
+            }
+        }
+
+        if let Some(mut final_cmd) = prev_cmd {
+            if let Err(e) = final_cmd.wait() {
+                eprintln!("{}", e);
             }
         }
     }
